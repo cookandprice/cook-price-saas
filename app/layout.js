4a1ba63cc6448cf1ccc4fec83a4f1fd0cf1fa3f9
Nabih <nabih.jebbari@live.fr>
2026-05-08 09:37:14 +0200

    Ajoute app avec formulaire

---
+++ app/layout.js
@@ -0,0 +1,8 @@
+export const metadata = { title: 'Cook Price SaaS' }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  )
+}
